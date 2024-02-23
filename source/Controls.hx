4a2bfb77d888c4e1e0051d2559b59031558392db
VaiLogoCaramba <150534279+VaiLogoCaramba@users.noreply.github.com>
2024-02-23 16:30:03 -0300

    Update Controls.hx

--- source/Controls.hx
+++ source/Controls.hx
@@ -586,7 +586,6 @@ class Controls extends FlxActionSet
 	{
 		var copyKeys:Array<FlxKey> = keys.copy();
 		for (i in 0...copyKeys.length) {
-			if(i == NONE) copyKeys.remove(i);
 		}
 
 		#if (haxe >= "4.0.0")
